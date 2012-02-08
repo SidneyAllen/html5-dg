fc263392f5b3ff2c129fe88ccaf9d860e66fe6ec
Sidney Maestre <sid@crushitmobile.com>
2012-02-08 08:26:35 -0800

    PHP-added javascript for mobile saving of Local Storage
    Check for parent window and save directly if no parent exists.  This is
    due to mobile not using the lightbox.

--- server/php/success.php
+++ server/php/success.php
@@ -3,6 +3,9 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Thank you</title>
+<script src="https://www.paypalobjects.com/js/external/dg.js"></script>
+<script src="../../client/jquery-1.6.2.min.js" type="text/javascript"></script>
+<script src="../../client/pptransact.js"></script>
 
 <?php
 require_once("pptransact.php");
@@ -13,17 +16,41 @@ $returnObj = $transact->commitPayment($data[1], $_GET["PayerID"], $_GET["token"]
 ?>
 
 <script>
-function closeFlow() {
-    parent.pptransact.releaseDG(<?= json_encode($returnObj); ?>);
+
+function parentExists() {
+ 	return (parent.location == window.location)? false : true;
+}
+
+function closeFlow(param) {
+	
+	pptransact.init('cf',true);
+	
+	if(!parentExists()) {
+		var jsonData = $.parseJSON('<?= $returnObj ?>');
+		pptransact.saveToLocalStorage(jsonData.userId,<?= $returnObj ?>,null);
+		
+		setTimeout ( forceCloseFlow, '3000' );
+		
+	} else {
+		parent.pptransact.releaseDG(<?= $returnObj ?>);
+	}
+  
+}
+
+function forceCloseFlow() {
+
+	//The page you want to redirect the user after successfully storing data in local storage.
+	window.location.href = '../../index.html';
+		
 }
 
 </script>
 </head>
 
-<body onload="closeFlow()">
-<div style="background-color:#FFF;height:700px;width:300px; border-radius:8px;padding:20px;">
-    Thank you for the purchase!<br />
-    <button id="close" onclick="closeFlow();">close</button>
+<body onload="closeFlow(false)">
+<div style="background-color:#FFF;height:400px;width:300px; border-radius:8px;padding:20px;">
+    Thank you for the purchase!  You will automatically return to your site in 3 seconds.
+    <button id="close" onclick="forceCloseFlow();">return now</button>
 </div>
 </body>
 </html>
